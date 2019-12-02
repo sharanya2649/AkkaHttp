229efe47e0f6f2321510789e9aaf430b3daa3c85
Sharanya <sharanya.m@techsophy.com>
2019-12-02 11:44:17 +0530

    first commit

--- src/main/scala/com/techsophy/akka/oauth2/KeycloakTokenVerifier.scala
+++ src/main/scala/com/techsophy/akka/oauth2/KeycloakTokenVerifier.scala
@@ -1,21 +1,25 @@
-package com.techsophy.akka.sso
+package com.techsophy.akka.oauth2
+
+import java.util.concurrent.ForkJoinPool
 
 import org.keycloak.RSATokenVerifier
 import org.keycloak.adapters.KeycloakDeployment
+import org.keycloak.representations.AccessToken
 
-import scala.concurrent.forkjoin.ForkJoinPool
 import scala.concurrent.{ExecutionContext, Future}
 
-class KeycloakTokenVerifier(keycloakDeployment: KeycloakDeployment) extends TokenVerifier {
+class KeycloakTokenVerifier(kId: String, keycloakDeployment: KeycloakDeployment) extends TokenVerifier {
   implicit val executionContext = ExecutionContext.fromExecutor(new ForkJoinPool(2))
 
-  def verifyToken(token: String): Future[String] = {
+  def verifyToken(token: String): Future[AccessToken] = {
     Future {
       RSATokenVerifier.verifyToken(
         token,
-        keycloakDeployment.getRealmKey,
+        keycloakDeployment.getPublicKeyLocator
+          .getPublicKey(kId, keycloakDeployment),
         keycloakDeployment.getRealmInfoUrl
-      ).getPreferredUsername
+      )
     }
   }
 }
+
--- src/main/scala/com/techsophy/akka/oauth2/OAuth2Authorization.scala
+++ src/main/scala/com/techsophy/akka/oauth2/OAuth2Authorization.scala
@@ -1,20 +1,20 @@
-package com.techsophy.akka.sso
+package com.techsophy.akka.oauth2
 
-import akka.event.LoggingAdapter
 import akka.http.scaladsl.model.headers.{Authorization, OAuth2BearerToken}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server._
+import org.keycloak.representations.AccessToken
 
-class OAuth2Authorization(logger: LoggingAdapter, tokenVerifier: TokenVerifier) {
+
+class OAuth2Authorization(tokenVerifier: TokenVerifier) {
 
   def authorized: Directive1[OAuth2Token] = {
     bearerToken.flatMap {
       case Some(token) =>
         onComplete(tokenVerifier.verifyToken(token)).flatMap {
-          _.map(username => provide(OAuth2Token(token, username)))
+          _.map(token => provide(OAuth2Token(token)))
             .recover {
               case ex =>
-                logger.error(ex, "Couldn't log in using provided authorization token")
                 reject(AuthorizationFailedRejection).toDirective[Tuple1[OAuth2Token]]
             }
             .get
@@ -26,8 +26,8 @@ class OAuth2Authorization(logger: LoggingAdapter, tokenVerifier: TokenVerifier) 
 
   private def bearerToken: Directive1[Option[String]] =
     for {
-      authBearerHeader  <- optionalHeaderValueByType(classOf[Authorization]).map(extractBearerToken)
-      xAuthCookie       <- optionalCookie("X-Authorization-Token").map(_.map(_.value))
+      authBearerHeader <- optionalHeaderValueByType(classOf[Authorization]).map(extractBearerToken)
+      xAuthCookie <- optionalCookie("X-Authorization-Token").map(_.map(_.value))
     } yield authBearerHeader.orElse(xAuthCookie)
 
   private def extractBearerToken(authHeader: Option[Authorization]): Option[String] =
@@ -37,4 +37,4 @@ class OAuth2Authorization(logger: LoggingAdapter, tokenVerifier: TokenVerifier) 
 
 }
 
-case class OAuth2Token(token: String, username: String)
+case class OAuth2Token(token: AccessToken)
